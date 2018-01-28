3ef572159aa986b484d3d11b3812e64ad773e3b5
GouBo <gb@hklh8.com>
2018-01-28 17:18:24 +0800

    关闭banner

--- src/main/java/com/hklh8/StartController.java
+++ src/main/java/com/hklh8/StartController.java
@@ -3,12 +3,10 @@ package com.hklh8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
 /**
  * Created by GouBo on 2018/1/28.
  */
-@Controller
 @SpringBootApplication
 @EnableAutoConfiguration
 public class StartController {
@@ -16,5 +14,4 @@ public class StartController {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(StartController.class, args);
     }
-
 }
