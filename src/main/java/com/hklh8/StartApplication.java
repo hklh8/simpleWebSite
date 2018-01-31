56cef54c7d52e3e46f3af219d8bbad99f2ae5bee
GouBo <gb@hklh8.com>
2018-01-31 16:57:46 +0800

    添加https支持。

--- src/main/java/com/hklh8/StartApplication.java
+++ src/main/java/com/hklh8/StartApplication.java
@@ -1,14 +1,12 @@
 package com.hklh8;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by GouBo on 2018/1/28.
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class StartApplication {
 
     public static void main(String[] args) throws Exception {
---
+++ src/main/java/com/hklh8/config/HttpsConfig.java
@@ -0,0 +1,53 @@
+package com.hklh8.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by GouBo on 2018/1/25.
+ */
+@Configuration
+public class HttpsConfig {
+
+    @Value("${server.port}")
+    private int httpsPort;
+
+    @Value("${http.port}")
+    private int httpPort;
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
+}
